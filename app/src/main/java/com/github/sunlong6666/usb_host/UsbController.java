884706cde4e5569dd063aa436e0fcc0dda3219fd
Sunlong <sunlong666@aliyun.com>
2020-10-12 09:41:23 +0800

    sunlong666@aliyun.com

--- app/src/main/java/com/github/sunlong6666/usb_host/UsbController.java
+++ app/src/main/java/com/github/sunlong6666/usb_host/UsbController.java
@@ -6,9 +6,12 @@ import android.hardware.usb.UsbManager;
 public interface UsbController
 {
 
+    //USB设备接入时的接口
     void onDeviceInsert(UsbMonitor usbMonitor, UsbManager usbManager, UsbDevice usbDevice);
 
+    //USB设备弹出时的接口
     void onDevicePullOut(UsbMonitor usbMonitor, UsbManager usbManager, UsbDevice usbDevice);
 
+    //USB设备连接成功时的接口
     void onDeviceOpen(UsbMonitor usbMonitor, UsbManager usbManager, UsbDevice usbDevice);
 }
